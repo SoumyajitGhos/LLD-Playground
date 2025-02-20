0e880d589b88ed733300d5ae65c787b044a57512
SoumyajitGhos <soumysjitghos@gmail.com>
2025-02-20 18:47:22 +0530

    Implementing and understanding SOLID principles logic :- SRP and OCP

---
+++ src/SolidPrinciples/OCP/Main.java
@@ -0,0 +1,17 @@
+package SolidPrinciples.OCP;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         *  Open-Closed Principle(OCP) :-
+         *  It states that according to new
+         *  requirements the module should be open for extension but
+         *  closed for modification.
+         *
+         *  Here we implemented a NotificationService interface
+         *  and respective classes to implements the base function
+         *  which will be different in its own way. Allowing it to open for extension
+         *  when trying to implement Twitter Notification service.
+         */
+    }
+}
---
+++ src/SolidPrinciples/SRP/Main.java
@@ -0,0 +1,15 @@
+package SolidPrinciples.SRP;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         *  Single Responsibility Principle(SRP) :-
+         *  It states that every java class must perform a single functionality.
+         *  Implementation of multiple functionalities in a single class mashup the code
+         *
+         *  All the individual classes have only 1 type of function to
+         *  perform so will be modified only when 1 type of task is to be altered
+         *  maintaining this principle
+         */
+    }
+}
