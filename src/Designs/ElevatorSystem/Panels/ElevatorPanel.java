e08d19a63c84316750b7db46dad7b1359c004816
Soumyajit Ghosh <soumyajit.ghosh@vaticahealth.com>
2026-04-17 19:55:25 +0530

    Adding Panels Classes

---
+++ src/Designs/ElevatorSystem/Panels/ElevatorPanel.java
@@ -0,0 +1,46 @@
+package Panels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Buttons.DoorButton;
+import Buttons.ElevatorButton;
+import Buttons.EmergencyButton;
+
+public class ElevatorPanel {
+    private final List<ElevatorButton> floorButtons;
+    private final DoorButton openButton = new DoorButton();
+    private final DoorButton closeButton = new DoorButton();
+    private final EmergencyButton emergencyButton = new EmergencyButton();
+
+    public ElevatorPanel(int numFloors) {
+        floorButtons = new ArrayList<>();
+        for (int i = 0; i < numFloors; i++) {
+            floorButtons.add(new ElevatorButton(i));
+        }
+    }
+
+    public List<ElevatorButton> getFloorButtons() {
+        return floorButtons;
+    }
+
+    public DoorButton getOpenButton() {
+        return openButton;
+    }
+
+    public DoorButton getCloseButton() {
+        return closeButton;
+    }
+
+    public EmergencyButton getEmergencyButton() {
+        return emergencyButton;
+    }
+
+    public void enterEmergency() {
+        emergencyButton.isPressed();
+    }
+
+    public void exitEmergency() {
+        emergencyButton.reset();
+    }
+}
---
+++ src/Designs/ElevatorSystem/Panels/HallPanel.java
@@ -0,0 +1,22 @@
+package Panels;
+
+import Buttons.HallButton;
+import Constants.Direction;
+
+public class HallPanel {
+    private final HallButton up;
+    private final HallButton down;
+
+    public HallPanel(int floorNumber, int topFloor) {
+        this.up = (floorNumber == topFloor) ? null : new HallButton(Direction.UP);
+        this.down = (floorNumber == 0) ? null : new HallButton(Direction.DOWN);
+    }
+
+    public HallButton getUpButton() {
+        return up;
+    }
+
+    public HallButton getDownButton() {
+        return down;
+    }
+}
